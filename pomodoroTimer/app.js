0d46e8c44d8b1f34cd8b429e623f7a983df46e10
Deltta <deltta.dev@gmail.com>
2024-09-29 23:06:50 -0300

    Erro que duplicava a velocidade do tempo ao clicar em start mais de uma vez consertado finalmente! Fiz muitas alterações na estética, mas falta fazer funcionar a barra circular de progresso ao redor do timer.

--- pomodoroTimer/app.js
+++ pomodoroTimer/app.js
@@ -3,8 +3,9 @@ const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
 const timer = document.getElementById("timer");
 
-let timeLeft = 1500; // 25min em segundos
+let timeLeft = 1500; // tempo que será decrementado
 let interval; // intervalo de alteração = 1s
+let isTimerRunning = false; // controla se o timer está rodando
 
 // função que atualiza o tempo na tela utilizando duas variáveis, minutos e segundos.
 const updateTimer = () => {
@@ -12,36 +13,38 @@ const updateTimer = () => {
     const seconds = timeLeft % 60;
 
     timer.innerHTML = 
-    `${minutes.toString().padStart(2,"0")}
-    :
-    ${seconds.toString().padStart(2,"0")}`;
+    `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
 };
 
 // função que diminui o tempo de segundo em segundo
 const startTimer = () => {
 
-    interval = setInterval(() => {
-        timeLeft--;
-        updateTimer();
-
-        // tem que ajeitar o problema do duplo click no botão start, que decrementa o tempo multiplicando
-
-        if(timeLeft <1500 && timeLeft > 0){
+    if(!isTimerRunning){
+        isTimerRunning = true;
+        interval = setInterval(() => {
+            timeLeft--;
             updateTimer();
-        }
 
-        if(timeLeft === 0){
-            clearInterval(interval);
-            alert("Hora de uma pausa!");
-            timeLeft = 1500;
-            updateTimer();
-        }
-    }, 1000);
+            if(timeLeft <1500 && timeLeft > 0){
+                updateTimer();
+            }
+
+            if(timeLeft === 0){
+                clearInterval(interval);
+                isTimerRunning = false; 
+                alert("Hora de uma pausa!");
+                timeLeft = 1500;
+                updateTimer();
+            }
+
+        }, 1000);
+    }
 };
 
 // função que congela o timer
 const stopTimer = () => {
     clearInterval(interval);
+    isTimerRunning = false;
 }
 
 // função que reseta o timer para seu valor padrão
@@ -49,6 +52,7 @@ const resetTimer = () => {
     clearInterval(interval);
     timeLeft = 1500;
     updateTimer();
+    isTimerRunning = false;
 }
 
 // definindo os eventos para cada botão
